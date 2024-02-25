3733ec4f4beaf4adac647fe629c0a7ffc2a1a3d9
charlescaseymartin <charlescaseymartin@gmail.com>
2024-02-25 03:47:04 +0200

    added dynamic username and password

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/sh 
 # Arch Linux installer script. EFI only! 
-# Parcevval @2023. 
+# Based on Parcevval @2023. 
+# Modified by Charles-Casey @2024. 
 [ -z "$1" ] && printf "Usage: Provide only the drive to install to (i.e /dev/sda, see lsblk)\n\n./archstrap.sh [DRIVE]\n\n" && exit 
 [ ! -b "$1" ] && printf "Drive $1 is not a valid block device.\n" && exit 
 
@@ -14,11 +15,11 @@ root=${disk}3
 
 # Cleanup from previous runs.
 [ -b "$swap" ] && swapoff $swap
-umount -R /mnt  
+umount -R /mnt
 
 # Partition 1G for boot, 1G for swap, rest for root. 
 # Optimal alignment will change the exact size though! 
-set -xe 
+set -xe
 parted -s $disk mklabel gpt 
 parted -sa optimal $disk mkpart primary fat32 0% 1G 
 parted -sa optimal $disk mkpart primary linux-swap 1G 2G 
@@ -36,14 +37,18 @@ mount --mkdir $boot /mnt/boot
 swapon $swap 
 
 # Packages and chroot. 
-pacstrap /mnt linux linux-firmware networkmanager vim base base-devel git man efibootmgr grub 
+pacstrap /mnt linux linux-firmware ufw networkmanager neovim base base-devel git man efibootmgr grub 
 
 genfstab -U /mnt > /mnt/etc/fstab 
 
 # Enter the system and set up basic locale, passwords and bootloader.
+read -s "Enter username: " username
+read -sp "Enter password: " password
+hostname="arch-clone"
 
 arch-chroot /mnt sh -c \
-	'set -xe; 
+	'
+	set -xe; 
 	sed -i "s/^#en_US.UTF-8/en_US.UTF-8/g" /etc/locale.gen; 
 	
 	echo "LANG=en_US.UTF-8" > /etc/locale.conf; 
@@ -51,14 +56,21 @@ arch-chroot /mnt sh -c \
 	ln -sf /usr/share/zoneinfo/Africa/Johannesburg /etc/localtime; 
 	hwclock --systohc;
 
+	ufw reset;
+	ufw default deny incoming;
+	ufw default allow outgoing;
+	ufw enable;
+
+	systemctl enable ufw; 
 	systemctl enable NetworkManager; 
-	echo root:123 | chpasswd; 
+	echo "$username:$password" | chpasswd; 
 	
-	echo "archer" > /etc/hostname; 
-	
-	mkdir /boot/grub; 
-	grub-mkconfig -o /boot/grub/grub.cfg; 
-	grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB;' 
+	echo "$hostname" > /etc/hostname;
+	echo -e "127.0.0.1	localhost.localdomain   localhost\n::1		localhost.localdomain   localhost\n127.0.0.1    $hostname.localdomain    $hostname" > /etc/hosts; 
+
+	grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB; 
+	grub-mkconfig -o /boot/grub/grub.cfg;
+	'
 
 # Finalize. 
 umount -R /mnt 
@@ -66,8 +78,8 @@ umount -R /mnt
 set +xe printf "
 	*--- Installation Complete! ---*
 	|                              |
-	|        Username: root        |
-	|        Password: 123         |
+	|     Username: $username      |
+	|     Password: $password      |
 	|                              |
 	*------------------------------*
 "
