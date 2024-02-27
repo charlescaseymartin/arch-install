f902a7a98aee1715e22a62d90cd66a1c06ecc022
charlescaseymartin <charlescaseymartin@gmail.com>
2024-02-27 17:05:42 +0200

    changed user creation vars

--- install.sh
+++ install.sh
@@ -39,13 +39,13 @@ swapon $swap
 hostname="arch-clone"
 
 read -s -p "Enter root user password: " rootpass
-[ -z $rootpass ] > /dev/null && printf "\nEnter valid root user password!" && exit
+[ -z "$rootpass" ] && printf "\nEnter valid root user password!" && exit
 
 read -p "Enter username: " username
-[ -z $username ] && printf "\nEnter valid username!" && exit
+[ -z "$username" ] && printf "\nEnter valid username!" && exit
 
 read -s -p "Enter user password: " userpass
-[ -z $userpass ] && printf "\nEnter valid user password!" && exit
+[ -z "$userpass" ] && printf "\nEnter valid user password!" && exit
 
 
 
