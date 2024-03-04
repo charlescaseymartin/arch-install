e5722723acf0080f8987e750b9bab1c1db5da823
charlescaseymartin <charlescaseymartin@gmail.com>
2024-03-04 21:47:08 +0200

    fix on vbox setup

--- install.sh
+++ install.sh
@@ -14,44 +14,44 @@ swap=${disk}1
 boot=${disk}2
 root=${disk}3
 
-## Cleanup from previous runs.
-#[ -b "$swap" ] && swapoff $swap
-#umount -R /mnt
-#
-## Partition 1G for boot, 1G for swap, rest for root.
-## Optimal alignment will change the exact size though!
-#parted -s $disk mklabel gpt
-#parted -sa optimal $disk mkpart primary linux-swap 0% 1G
-#parted -sa optimal $disk mkpart primary fat32 1G 2G
-#parted -sa optimal $disk mkpart primary ext4 2G 100%
-#parted -s $disk set 2 esp on
-#
-## Format the partitions.
-#mkfs.ext4 -F $root
-#mkfs.fat -F 32 $boot
-#mkswap $swap
-#
-## Mount the partitions.
-#mount $root /mnt
-#mount --mkdir $boot /mnt/boot
-#swapon $swap
-#
-#archinstall --config ./config.json --creds ./creds.json
-#
-## Install configs and environment
-#arch-chroot /mnt sh -c 'systemctl enable ufw.service'
-#arch-chroot /mnt sh -c 'systemctl enable NetworkManager'
-#arch-chroot /mnt sh -c 'chsh -s $(which zsh)'
-#arch-chroot /mnt sh -c 'curl https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh | sh'
-#arch-chroot /mnt sh -c 'cd /root; git clone https://github.com/charlescaseymartin/archlinux-moded-dotfiles.git'
-#arch-chroot /mnt sh -c 'cd /root/archlinux-moded-dotfiles; sh install.sh -i'
+# Cleanup from previous runs.
+[ -b "$swap" ] && swapoff $swap
+umount -R /mnt
+
+# Partition 1G for boot, 1G for swap, rest for root.
+# Optimal alignment will change the exact size though!
+parted -s $disk mklabel gpt
+parted -sa optimal $disk mkpart primary linux-swap 0% 1G
+parted -sa optimal $disk mkpart primary fat32 1G 2G
+parted -sa optimal $disk mkpart primary ext4 2G 100%
+parted -s $disk set 2 esp on
+
+# Format the partitions.
+mkfs.ext4 -F $root
+mkfs.fat -F 32 $boot
+mkswap $swap
+
+# Mount the partitions.
+mount $root /mnt
+mount --mkdir $boot /mnt/boot
+swapon $swap
+
+archinstall --config ./config.json --creds ./creds.json
+
+# Install configs and environment
+arch-chroot /mnt sh -c 'systemctl enable ufw.service'
+arch-chroot /mnt sh -c 'systemctl enable NetworkManager'
+arch-chroot /mnt sh -c 'chsh -s $(which zsh)'
+arch-chroot /mnt sh -c 'curl https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh | sh'
+arch-chroot /mnt sh -c 'cd /root; git clone https://github.com/charlescaseymartin/archlinux-moded-dotfiles.git'
+arch-chroot /mnt sh -c 'cd /root/archlinux-moded-dotfiles; sh install.sh -i'
 
 # Checks if virtual machine argument is valid
 if [ "$2" == "-v" ]
 then
         printf "\nConfiguring virtualbox environment..."
-        #arch-chroot /mnt sh -c 'pacman -S virtualbox-guest-utils --noconfirm; systemctl enable vboxservice.service'
-        #arch-chroot /mnt sh -c 'VBoxClient --clipboard; VBoxClient --seamless'
+        arch-chroot /mnt sh -c 'pacman -S virtualbox-guest-utils --noconfirm; systemctl enable vboxservice.service'
+        arch-chroot /mnt sh -c 'VBoxClient --clipboard; VBoxClient --seamless'
 fi
 
 printf "*--- Installation Complete! ---*"
